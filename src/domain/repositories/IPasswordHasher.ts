76d72b9e36fae4e0ee139668f56a37e5808a77ca
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-06-26 19:30:32 -0300

    Create PasswordHasher interface

---
+++ src/domain/repositories/IPasswordHasher.ts
@@ -0,0 +1,6 @@
+interface IPasswordHasher {
+  hashPassword(password: string): Promise<string>;
+  comparePassword(password: string, hashedPassword: string): Promise<boolean>;
+}
+
+export default IPasswordHasher;
