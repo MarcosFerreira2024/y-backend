f7ac0c144ec40edfe4deac4daff8a2fe56da5b14
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-06-28 19:25:11 -0300

    Update UserFactory to use UserDataDTO and add slug support

--- src/domain/entities/factories/UserFactory.ts
+++ src/domain/entities/factories/UserFactory.ts
@@ -2,26 +2,29 @@ import IPasswordHasher from "../../repositories/IPasswordHasher";
 import PasswordService from "../../services/PasswordService";
 import Email from "../../value-objects/Email";
 import Name from "../../value-objects/Name";
-import Id from "../../value-objects/UserId";
+import Slug from "../../value-objects/Slug";
+import Id from "../../value-objects/Id";
 import User from "../User";
 
-type UserData = {
-  id: string;
+type UserDataDTO = {
+  id: number;
   name: string;
   email: string;
   password: string;
   profile_picture: string | null;
   profile_bg: string | null;
   created_at: Date;
+  slug: string;
   updated_at: Date;
 };
 
-async function UserFactory(data: UserData, hasher: IPasswordHasher) {
+async function UserFactory(data: UserDataDTO, hasher: IPasswordHasher) {
   const passwordService = new PasswordService(hasher);
   const password = await passwordService.create(data.password);
   return new User(
     Id.create(data.id),
     Name.create(data.name),
+    Slug.create(Name.create(data.name)),
     Email.create(data.email),
     password,
     data.profile_picture,
@@ -33,4 +36,4 @@ async function UserFactory(data: UserData, hasher: IPasswordHasher) {
 
 export default UserFactory;
 
-export type { UserData };
+export type { UserDataDTO };
