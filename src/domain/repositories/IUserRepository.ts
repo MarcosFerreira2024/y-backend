b3df1e897a77c7ad79659998382a2af612af7af5
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-07-01 19:50:07 -0300

    Update IUserRepository.ts

--- src/domain/repositories/IUserRepository.ts
+++ src/domain/repositories/IUserRepository.ts
@@ -1,27 +1,43 @@
-import { UserDataDTO } from "../entities/factories/UserFactory";
 import User from "../entities/User";
 
+export type UserCreationInputs = {
+  email: string;
+  name: string;
+  password: string;
+  slug: string;
+};
+
+export type UserData = {
+  slug: string;
+  name: string;
+  id: number;
+  email: string;
+  password: string;
+  profile_picture: string | null;
+  profile_bg: string | null;
+  created_at: Date;
+  updated_at: Date;
+} | null;
+
 interface IUserRepository {
-  create(user: UserDataDTO): Promise<void>;
-  findByEmail(email: string): Promise<UserDataDTO | null>;
-  findById(id: number): Promise<UserDataDTO | null>;
-  findBySlug(slug: string): Promise<UserDataDTO | null>;
+  create(data: UserCreationInputs): Promise<void>;
+  findByEmail(email: string): Promise<UserData | null>;
+  findById(id: number): Promise<UserData | null>;
+  findBySlug(slug: string): Promise<UserData | null>;
   updateProfilePicture(
     id: number,
     profile_picture: string
-  ): Promise<Omit<UserDataDTO, "password">>;
+  ): Promise<Omit<UserData, "password">>;
   updateProfileBg(
     id: number,
     profile_bg: string
-  ): Promise<Omit<UserDataDTO, "password">>;
-  updateEmail(
-    id: number,
-    email: string
-  ): Promise<Omit<UserDataDTO, "password">>;
+  ): Promise<Omit<UserData, "password">>;
+  updateEmail(id: number, email: string): Promise<Omit<UserData, "password">>;
+  updateName(id: number, name: string): Promise<Omit<UserData, "password">>;
   updatePassword(
     id: number,
     newPassword: string
-  ): Promise<Omit<UserDataDTO, "password">>;
+  ): Promise<Omit<UserData, "password">>;
   delete(id: number): Promise<void>;
 }
 
