d0a17a50d7b7c047771fb4c2ebfb0760600d48b1
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-07-01 19:50:10 -0300

    Update IPostRepository.ts

--- src/domain/repositories/IPostRepository.ts
+++ src/domain/repositories/IPostRepository.ts
@@ -1,13 +1,28 @@
-import Post, { PostDataDTO } from "../entities/Post";
+export type PostData = {
+  id: number;
+  image: string | null;
+  content: string;
+  likes_count: number;
+  created_at: Date;
+  updated_at: Date;
+  user_id: number;
+};
+
+export type PostCreationInputs = {
+  user_id: number;
+  content: string;
+  image?: string | null;
+};
 
 interface IPostRepository {
-  create(post: PostDataDTO): Promise<void>;
+  create(post: PostCreationInputs): Promise<void>;
   delete(id: number): Promise<void>;
+  findById(id: number): Promise<PostData | null>;
   addLike(post_id: number, user_id: number): Promise<void>;
   removeLike(post_id: number, user_id: number): Promise<void>;
   getLikes(post_id: number): Promise<number>;
-  findByContent(content: string): Promise<PostDataDTO[] | null>;
-  findByUserSlug(slug: string): Promise<PostDataDTO[] | null>;
+  findByContent(content: string): Promise<PostData[] | null>;
+  findByUserSlug(slug: string): Promise<PostData[] | null>;
 }
 
 export default IPostRepository;
