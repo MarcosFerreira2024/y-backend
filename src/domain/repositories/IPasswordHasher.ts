e9baa5bc5f6d6840f5376e9b720c13362c4c56bb
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-07-01 19:50:05 -0300

    Update IPasswordHasher.ts

--- src/domain/repositories/IPasswordHasher.ts
+++ src/domain/repositories/IPasswordHasher.ts
@@ -2,7 +2,7 @@ import Password from "../value-objects/Password";
 
 interface IPasswordHasher {
   hashPassword(password: string): Promise<string>;
-  comparePassword(password: string, hashedPassword: Password): Promise<boolean>;
+  comparePassword(password: string, user_password: string): Promise<boolean>;
 }
 
 export default IPasswordHasher;
