b5c9f65fa052d4417310754ecf693f00eb4925e3
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-07-02 22:16:21 -0300

    Add notification on post like in LikePostUseCase

--- src/application/useCases/Post/LikePostUseCase.ts
+++ src/application/useCases/Post/LikePostUseCase.ts
@@ -1,25 +1,31 @@
 import { inject, injectable } from "tsyringe";
 import IPostRepository from "../../../domain/repositories/IPostRepository";
+import INotificationService from "../../../domain/services/INotificationService";
+import { getSocketInstance } from "../../../infrastructure/configs/socket/socketInstance";
+import { emitNotification } from "../../../infrastructure/helper/emitNotification";
 
 @injectable()
 class LikePostUseCase {
   constructor(
-    @inject("PostRepository") private postRepository: IPostRepository
+    @inject("PostRepository") private postRepository: IPostRepository,
+    @inject("NotificationService")
+    private notificationService: INotificationService
   ) {}
 
-  async execute(
-    post_id: number,
-    user_id: number
-  ): Promise<{ message: string }> {
+  async execute(post_id: number, user_id: number): Promise<void> {
     const post = await this.postRepository.findById(post_id);
     if (!post) {
-      throw new Error("Post not found");
+      throw new Error("Post not found or the post was deleted");
     }
     await this.postRepository.addLike(post_id, user_id);
 
-    return {
-      message: "Post liked successfully",
-    };
+    const notification = await this.notificationService.create(
+      post.author.id,
+      `Someone liked your post`,
+      "POST_LIKED"
+    );
+
+    await emitNotification(notification, post.author.id);
   }
 }
 
