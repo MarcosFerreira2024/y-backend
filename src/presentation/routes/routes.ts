451df998fe3580ea1d55bf6bb60b11c59438c847
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-07-02 22:20:49 -0300

    Update routes.ts

--- src/presentation/routes/routes.ts
+++ src/presentation/routes/routes.ts
@@ -7,6 +7,6 @@ const routes = Router();
 
 routes.use("/users", user_routes);
 routes.use("/auth", auth_routes);
-routes.use("/posts", post_routes);
+routes.use("/post", post_routes);
 
 export { routes };
