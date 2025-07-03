f4469983f49bfc8493838aac0ff84a00cd1416ce
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-07-02 22:16:42 -0300

    Add notification on user name update

--- src/application/useCases/User/UpdateUserNameUseCase.ts
+++ src/application/useCases/User/UpdateUserNameUseCase.ts
@@ -2,11 +2,16 @@ import { inject, injectable } from "tsyringe";
 import IUserRepository, {
   UserData,
 } from "../../../domain/repositories/IUserRepository";
+import INotificationService from "../../../domain/services/INotificationService";
+import { emitNotification } from "../../../infrastructure/helper/emitNotification";
+import Name from "../../../domain/value-objects/Name";
 
 @injectable()
 class UpdateUserNameUseCase {
   constructor(
-    @inject("UserRepository") private userRepository: IUserRepository
+    @inject("UserRepository") private userRepository: IUserRepository,
+    @inject("NotificationService")
+    private notificationService: INotificationService
   ) {}
 
   async execute(
@@ -18,7 +23,17 @@ class UpdateUserNameUseCase {
     if (!user) {
       throw new Error("User not found");
     }
-    return await this.userRepository.updateName(id, name);
+    const updated = await this.userRepository.updateName(id, name);
+
+    const notification = await this.notificationService.create(
+      id,
+      `${Name.formatter(user.name)} your name has been updated`,
+      "NAME_CHANGED"
+    );
+
+    await emitNotification(notification, id);
+
+    return updated;
   }
 }
 
