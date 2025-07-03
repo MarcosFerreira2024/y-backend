51ba91ab0fa89772a1b87a6c08ea0055ec01606b
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-07-02 22:20:30 -0300

    Add socket.io integration and refactor server setup

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,24 @@
 import express from "express";
-import { configureExpress } from "./infrastructure/configs/express.config";
+import "reflect-metadata";
 import { routes } from "./presentation/routes/routes";
+import http from "http";
+import dotenv from "dotenv";
+import { configureSocket } from "./infrastructure/configs/socket/socket";
+import { configureExpress } from "./infrastructure/configs/express";
+import { setSocketInstance } from "./infrastructure/configs/socket/socketInstance";
+dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
 configureExpress(app);
+const io = configureSocket(server);
+
+setSocketInstance(io);
 
 app.use(routes);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
