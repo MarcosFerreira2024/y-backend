3ce065a4ff515ce0d001c5b8b6b71c78764092e7
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-07-02 22:20:46 -0300

    Update user.routes.ts

--- src/presentation/routes/user.routes.ts
+++ src/presentation/routes/user.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
+import middleware from "../../infrastructure/middleware/middleware";
 
 const user_routes = Router();
+
+user_routes.use(middleware);
 
 user_routes.put(
   "/user/:id/profile_picture",
