d51cb8d5d451bfa0bc2d60653d46707e0db14a56
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-07-02 22:20:12 -0300

    Refactor post routes and add new endpoints

--- src/presentation/routes/post.routes.ts
+++ src/presentation/routes/post.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import PostController from "../controllers/PostController";
+import middleware from "../../infrastructure/middleware/middleware";
 
 const post_routes = Router();
 
-post_routes.post("/posts", PostController.create);
-post_routes.delete("/posts/:id", PostController.delete);
-post_routes.post("/posts/:id/like", PostController.like);
-post_routes.delete("/posts/:id/like", PostController.unlike);
+post_routes.use(middleware);
+post_routes.post("/", PostController.create);
+post_routes.delete("/:id", PostController.delete);
+post_routes.put("/:id/like", PostController.like);
+post_routes.put("/:id/unlike", PostController.unlike);
+post_routes.get("/:id", PostController.findById);
+post_routes.get("/user/:slug", PostController.findByUserSlug);
+post_routes.get("/content/:content", PostController.FindByContent);
 
 export { post_routes };
